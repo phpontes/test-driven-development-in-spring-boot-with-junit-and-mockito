1d975ecc316c0279e60c8ff3c622a0431c468e97
Paulo Henrique Pontes <115827846+phpontes@users.noreply.github.com>
2025-05-07 11:34:06 +0000

    Refactoring the data layer using Lombok

--- taskmanager/src/main/java/com/linkedin/taskmanager/model/Task.java
+++ taskmanager/src/main/java/com/linkedin/taskmanager/model/Task.java
@@ -4,8 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Task {
     
     @Id
@@ -14,23 +20,9 @@ public class Task {
     private String title;
     private String status;
 
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    
-    public String getTitle() {
-        return title;
-    }
-    public void setTitle(String title) {
+    public Task(String title, String status) {
         this.title = title;
-    }
-    public String getStatus() {
-        return status;
-    }
-    public void setStatus(String status) {
         this.status = status;
     }
+
 }
--- taskmanager/src/test/java/com/linkedin/taskmanager/repository/TaskRepositoryTest.java
+++ taskmanager/src/test/java/com/linkedin/taskmanager/repository/TaskRepositoryTest.java
@@ -21,9 +21,7 @@ public class TaskRepositoryTest {
     @Test
     void testSaveTask() {
         // arrange
-        Task task = new Task();
-        task.setTitle("Test task");
-        task.setStatus("To do");
+        Task task = new Task("Test task", "To do");
 
         // act
         Task savedTask = taskRepository.save(task);
@@ -36,9 +34,7 @@ public class TaskRepositoryTest {
     @Test
     void testDeleteTask() {
         // arrange
-        Task task = new Task();
-        task.setTitle("Task to delete");
-        task.setStatus("Done");
+        Task task = new Task("Task to delete", "Done");
         taskRepository.save(task);
 
         // act
