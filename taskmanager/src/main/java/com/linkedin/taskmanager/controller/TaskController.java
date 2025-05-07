1957e2620a4f768939ed2032413b17d50cd9cb6d
Paulo Henrique Pontes <115827846+phpontes@users.noreply.github.com>
2025-05-07 22:12:38 +0000

    Implementing the method of the controller

--- taskmanager/src/main/java/com/linkedin/taskmanager/controller/TaskController.java
+++ taskmanager/src/main/java/com/linkedin/taskmanager/controller/TaskController.java
@@ -1,8 +1,25 @@
 package com.linkedin.taskmanager.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.linkedin.taskmanager.model.Task;
+import com.linkedin.taskmanager.service.TaskService;
+
 @RestController
+@RequestMapping("/tasks")
 public class TaskController {
     
+    @Autowired
+    private TaskService taskService;
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        List<Task> tasks = taskService.getAllTasks();
+        return tasks;
+    }
 }
