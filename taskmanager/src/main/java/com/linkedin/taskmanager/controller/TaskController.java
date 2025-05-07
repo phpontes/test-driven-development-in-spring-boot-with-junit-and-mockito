3f3a88ff382cabbce5bdebd01b7983316729ac03
Paulo Henrique Pontes <115827846+phpontes@users.noreply.github.com>
2025-05-07 22:14:56 +0000

    Refactoring the first method of the controller

--- taskmanager/src/main/java/com/linkedin/taskmanager/controller/TaskController.java
+++ taskmanager/src/main/java/com/linkedin/taskmanager/controller/TaskController.java
@@ -2,7 +2,6 @@ package com.linkedin.taskmanager.controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,13 +12,14 @@ import com.linkedin.taskmanager.service.TaskService;
 @RestController
 @RequestMapping("/tasks")
 public class TaskController {
-    
-    @Autowired
     private TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
 
     @GetMapping
     public List<Task> getAllTasks() {
-        List<Task> tasks = taskService.getAllTasks();
-        return tasks;
+        return taskService.getAllTasks();
     }
 }
