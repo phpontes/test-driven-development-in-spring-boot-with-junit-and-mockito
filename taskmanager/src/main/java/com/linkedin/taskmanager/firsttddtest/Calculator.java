0f9f18b472d6fe9666a1c5a0b41bca98977e0abb
Paulo Henrique Pontes <115827846+phpontes@users.noreply.github.com>
2025-05-07 10:08:36 +0000

    Repeat: adding more features

--- taskmanager/src/main/java/com/linkedin/taskmanager/firsttddtest/Calculator.java
+++ taskmanager/src/main/java/com/linkedin/taskmanager/firsttddtest/Calculator.java
@@ -3,6 +3,10 @@ package com.linkedin.taskmanager.firsttddtest;
 public class Calculator {
     
     public double divide(double a, double b) {
-        return a / b;
+        if (b == 0) {
+            throw new ArithmeticException("Cannot divide by 0.");
+        } else {
+            return a / b;
+        }
     }
 }
--- taskmanager/src/test/java/com/linkedin/taskmanager/firsttddtest/CalculatorTest.java
+++ taskmanager/src/test/java/com/linkedin/taskmanager/firsttddtest/CalculatorTest.java
@@ -1,6 +1,7 @@
 package com.linkedin.taskmanager.firsttddtest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
@@ -15,6 +16,19 @@ public class CalculatorTest {
         double result = calculator.divide(6, 2);
 
         // assert
+        assertEquals(3.0, result);
+    }
+
+    @Test
+    void testDivideByZero() {
+        Calculator calculator = new Calculator();
+        assertThrows(ArithmeticException.class, () -> calculator.divide(5, 0));
+    }
+
+    @Test
+    void testDivideNegativeNumbers() {
+        Calculator calculator = new Calculator();
+        double result = calculator.divide(-6, -2);
         assertEquals(3.0, result);
     }
 }
