7662d7ca19c02338dcd355c3cefa0eb8bf670be5
Paulo Henrique Pontes <115827846+phpontes@users.noreply.github.com>
2025-05-07 18:52:11 +0000

    Implementing the method in the service layer

--- taskmanager/src/main/java/com/linkedin/taskmanager/service/TaskService.java
+++ taskmanager/src/main/java/com/linkedin/taskmanager/service/TaskService.java
@@ -1,10 +1,27 @@
 package com.linkedin.taskmanager.service;
 
-import com.linkedin.taskmanager.model.Task;
+import java.util.Optional;
 
+import org.springframework.stereotype.Service;
+
+import com.linkedin.taskmanager.model.Task;
+import com.linkedin.taskmanager.repository.TaskRepository;
+
+@Service
 public class TaskService {
-    
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
     public Task updateTaskStatus(Long id, String status){
+        Optional<Task> taskOptional = taskRepository.findById(id);
+        if (taskOptional.isPresent()) {
+            Task t = taskOptional.get();
+            t.setStatus(status);
+            return taskRepository.save(t);
+        }
         return null;
     }
 }
